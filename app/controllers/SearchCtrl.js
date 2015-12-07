cf2242713774c6fce8dfce509b4bd44c2e010ef7
Kirk Graves <graves.kirk@gmail.com>
2015-12-07 09:03:54 -0600

    added material design for BS styling and cast key to newtvShow object

--- app/controllers/SearchCtrl.js
+++ app/controllers/SearchCtrl.js
@@ -3,10 +3,10 @@ app.controller('SearchCtrl', ['$http', 'userFactory', function($http, userFactor
 	var self = this;
 	var pendingTask;
 
-
 	self.addTvShow = function() {
 		var newTvShow = {
 			title: self.tvData.Title,
+			cast: self.tvData.Actors,
 			plot: self.tvData.Plot,
 			year: self.tvData.Year,
 			imdbID: self.tvData.imdbID,
@@ -31,7 +31,6 @@ app.controller('SearchCtrl', ['$http', 'userFactory', function($http, userFactor
 		});
 	}
 
-
 	self.update = function(tvShow) {
 		self.search = tvShow.Title;
 		self.change();
@@ -41,31 +40,11 @@ app.controller('SearchCtrl', ['$http', 'userFactory', function($http, userFactor
 		if (pendingTask) {
 			clearTimeout(pendingTask);
 		}
-		pendingTask = setTimeout(fetch, 800);
+		pendingTask = setTimeout(fetch, 500);
 	};
 
 	self.select = function(){
   	this.setSelectionRange(0, this.value.length);
 	};
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 }]); // End SearchCtrl
